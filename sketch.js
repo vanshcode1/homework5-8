0a0e39ecac36585a5996cb1c5b281c923d037e45
vanshcode1 <104743980+vanshcode1@users.noreply.github.com>
2022-05-08 18:12:47 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,8 @@ var seaImg,shipImg;
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1");
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+
   
   seaImg = loadImage("sea.png");
 }
@@ -36,10 +33,9 @@ function draw() {
 
   //uncomment code to reset the background
   if(sea.x < 0){
-    sea.x = 0;
-    sea.x = sea.width;
+
     sea.x = sea.width/8;
-    sea.y = height;
+
   }
 
  
